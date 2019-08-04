dfb481432e190c69ed72a439dbe9dc13293ecb24
eldadfux <eldad.fux@gmail.com>
2019-08-04 16:16:51 +0300

    Updated function credit

--- src/direction.js
+++ src/direction.js
@@ -7,7 +7,7 @@
             repeat: false,
             controller: function(element) {
                 let special = ['\n',' ', '״', '"', '_', '\'', '!', '@', '#', '$', '^', '&', '%', '*', '(', ')', '+', '=', '-', '[', ']', '\\', '/', '{', '}', '|', ':', '<', '>', '?', ',', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-                let isRTL = function(s) {
+                let isRTL = function (s) { // Credit for isRTL function: https://stackoverflow.com/a/14824756/2299554
                     let ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF' + '\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF',
                         rtlChars = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC',
                         rtlDirCheck = new RegExp('^[^' + ltrChars + ']*[' + rtlChars + ']');
